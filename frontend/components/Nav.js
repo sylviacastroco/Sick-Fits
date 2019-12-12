22afdb2ad17b565b1f7842633e2c7ad4d8fe7ad9
Sylvia <sylviacastro@Sylvias-MacBook-Pro.local>
2019-12-11 18:58:51 -0600

    added styled components and pages

---
+++ frontend/components/Nav.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import NavStyles from './styles/NavStyles'
+
+const Nav = () => (
+  <NavStyles>
+    <Link href="/sell">
+      <a>Items</a>
+    </Link>
+    <Link href="/">
+      <a>Sell</a>
+    </Link>
+    <Link href="/">
+      <a>Signup</a>
+    </Link>
+    <Link href="/">
+      <a>Orders</a>
+    </Link>
+    <Link href="/">
+      <a>Account</a>
+    </Link>
+
+
+  </NavStyles>
+)
+
+export default Nav;
