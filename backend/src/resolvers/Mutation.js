f0db4b0e0a1d285fe8218c00757b4d8ac2487491
Sylvia <sylviacastro@Sylvias-MacBook-Pro.local>
2019-12-14 14:58:04 -0600

    updated package.json

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,16 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info){
+    // TODO: Check if they're logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data:{
+        ...args
+      }
+    }, info);
+
+    console.log(item);
+    return item
+  }
+};
 
 module.exports = Mutations;
