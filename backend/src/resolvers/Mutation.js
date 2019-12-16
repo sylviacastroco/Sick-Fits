067d1a2d4a7a7f31fd88ea7be42cf216284b9d4b
Sylvia <sylviacastro527@gmail.com>
2019-12-15 19:11:34 -0600

    added delete functionality

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -23,7 +23,17 @@ const Mutations = {
         id: args.id
       }
     },
-    info);
+    info
+    );
+  },
+  async deleteItem(parent, args, ctx, info){
+    const where = {id: args.id};
+    //1. Find the item
+    const item = await ctx.db.query.item({where}, `{ id, title }`);
+    //2. Check if they own the item, or have the permissions
+   // TODO 
+    //3. Delete it!
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
